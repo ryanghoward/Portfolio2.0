95a71d9555a59576bbab8754ac2ca2b5c278e898
Ryan Howard <ryanghoward@protonmail.com>
2022-11-14 13:08:51 -0800

    About

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-scroll";
+import aboutImg from "../assets/code7.jpg";
+import { BsArrowRight } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className='h-[73vh] sm:h-[75.5vh]'>
+      <div className='container flex items-center justify-center mx-auto'>
+        <div className='relative flex flex-col items-centergap-24 lg:items-center xl:flex-row'>
+          <img
+            className='object-cover h-full max-w-[566px] hidden xl:flex lg:mx-0 rounded-2xl z-10'
+            src={aboutImg}
+            alt='About'
+            loading='lazy'
+          />
+          <div className='flex flex-col items-center text-center ml-10 h-[31em] justify-center lg:items-center lg:text-left'>
+            <div className='flex flex-col'>
+              <h2 className='text-3xl text-accent-hover lg:text-4xl font-medium lg:font-light mb-3 before:content-about relative before:absolute before:opacity-80 before:-top-[2.3rem] before:-left-[1rem] before:hidden before:lg:block'>
+                <span className='opacity-80'>Ryan G. Howard</span>
+              </h2>
+              <p className='mb-4'>Full-Stack Web Developer</p>
+              <hr className='mb-8' />
+              <p className='xs:w-[90vw] sm:w-auto mb-8'>
+                Welcome to my website! Thanks for stopping by. Over the past
+                year I chose to make a career change and dove head first into
+                the world of Web Development and Coding. I completed the Full
+                Stack Web Development boot camp at Bloom Institute of Technology
+                and loved every minute of it. This site is the culmination of my
+                work up to this point. Enjoy!
+              </p>
+              <Link to='projects'>
+                <button>
+                  See More <BsArrowRight size={25} />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-scroll";
 import myFace from "../assets/Ryan.PNG";
+import me from "../assets/me.jpg";
 
 const Hero = () => {
   return (
@@ -8,14 +9,14 @@ const Hero = () => {
       <div className='container h-full mx-auto'>
         <div className='flex items-center h-full pt-8'>
           <div className='flex flex-col items-center flex-1 lg:items-start'>
-            <p className='text-2xl text-accent-brown font-medium text-md mb-[22px]'>
+            <p className='text-2xl text-white font-medium text-md mb-[22px]'>
               Hello
-              <span className='font-light text=paragraph'>, I'm Ryan!</span>
+              <span className='font-light'>, I'm Ryan!</span>
             </p>
-            <h1 className='text-[#421650] text-center text-4xl leading-[44px] sm:text-center md:text-5xl md:leading-tight md:text-left lg:text-5xl lg:leading-[1.2] font-normal font-body md:tracking-[2px]'>
+            <h1 className='text-white text-center text-4xl leading-[44px] sm:text-center md:text-5xl md:leading-tight md:text-left lg:text-5xl lg:leading-[1.2] font-normal font-body md:tracking-[2px]'>
               I design and create websites and apps.
             </h1>
-            <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
+            <p className='text-white pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
               I am a full-stack developer (specializing in front-end
               development) who creates websites, applications, and games for a
               range of different customers.
@@ -26,18 +27,17 @@ const Hero = () => {
               smooth={true}
               duration={1100}
               offset={-70}
-              className='flex items-center justify-between transition-all duration-300 border cursor-pointer text-paragraph btn btn-md border-accent-brown hover:bg-accent-brownHover hover:text-primary md:btn-lg'
+              className='flex items-center justify-between transition-all duration-300 border cursor-pointer text-paragraph'
             >
               Let's build something together!
             </Link>
           </div>
           <div className='relative items-center justify-center flex-1 hidden h-full lg:flex'>
-            <div className='absolute h-[27em] w-[55%] lg:bottom-[19%] lg:right-[9%] lg:w-[19em] xl:w-[20em] xl:right-[15%] xl:bottom-[19%] rounded-2xl bg-tertiary opacity-30 z-1'></div>
             <img
               className='h-[55%] rounded-full z-10'
-              src={myFace}
-              alt=''
-              loading='lazy'
+              src={me}
+              alt='My Ugly Mug'
+              // loading='lazy'
             />
           </div>
         </div>
